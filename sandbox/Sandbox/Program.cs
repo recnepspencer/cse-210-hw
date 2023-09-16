5f9a21e95f64767b36dcf1c22914f7cc4a3b599f
Spencer Hepworth <goldenspencerh@gmail.com>
2023-09-16 15:02:29 -0600

    made functions for asking name and calculating grades then added them to the main function

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        AskForNames();
+
+        Console.WriteLine("What is your score?");
+        int score = int.Parse(Console.ReadLine());
+
+        string grade = CalculateGrade(score);
+        Console.WriteLine($"Your grade is {grade} ({score}%).");
+
     }
+
+    static void AskForNames(){
+        // Here we ask for the first name. We dont do write line because we want the user to type on the same line as the question.
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Here we ask for the last name. We dont do write line because we want the user to type on the same line as the question.
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Here we print out the name.
+        Console.WriteLine($"Your name is {firstName} {lastName}.");
+    }
+
+    static string CalculateGrade(int score){
+        string[] grades = {"A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-", "F"};
+        int[] cutoffs = {93, 90, 87, 83, 80, 77, 73, 70, 67, 63, 60, 0};
+
+        for(int i = 0; i < cutoffs.Length; i++){
+            if(score >= cutoffs[i]){
+                return grades[i];
+            }
+        }
+
+        return "There was an error.";
+    }
+    
 }
